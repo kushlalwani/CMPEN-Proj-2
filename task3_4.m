b1d2d50de73dc3f06d19f4e6f970b31a48873973
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-10-28 22:01:55 -0400

    Update task3_4.m
    Standardize variable naming conventions. Simplify vector computations. Compute normal vectors using SVD instead of the built-in cross function. Update floor plane verification.

--- task3_4.m
+++ task3_4.m
@@ -10,54 +10,46 @@ function task3_4()
     % Specify the 3D locations of 3 points on the wall
     wallPoints = triangulationData([6, 9, 27], :);
 
-    % Compute centroid
+    % Compute centroids
     floorCentroid = mean(floorPoints, 1);
     wallCentroid = mean(wallPoints, 1);
 
-    % Compute vectors in the floor plane
-    floorVectors = [
-      floorPoints(1, :) - floorCentroid;
-      floorPoints(2, :) - floorCentroid;
-      floorPoints(3, :) - floorCentroid
-    ];
+    % Subtract centroids from points
+    floorVectors = floorPoints - floorCentroid;
+    wallVectors = wallPoints - wallCentroid;
 
-    % Compute vectors in the wall plane
-    wallVectors = [
-      wallPoints(1, :) - wallCentroid;
-      wallPoints(2, :) - wallCentroid;
-      wallPoints(3, :) - wallCentroid
-    ];
+    % Derive orthonormal vectors
+    [~, ~, fV] = svd(floorVectors, 0);
+    [~, ~, wV] = svd(wallVectors, 0);
 
-    % Compute the normal vector of the plane
-    normalVector = cross(floorVectors(1, :), floorVectors(2, :));
-    normalVectorW = cross(wallVectors(1, :), wallVectors(2, :));
+    % Derive normal vectors
+    floorNormal = fV(:, 3);
+    wallNormal = wV(:, 3);
 
-    % Normalize normal vector
-    normalVector = normalVector / norm(normalVector);
-    normalVectorW = normalVectorW / norm(normalVectorW);
-    
-    % Compute the constant d
-    d = -dot(normalVector, floorCentroid);
-    dW = -dot(normalVectorW, wallCentroid);
+    % Normalize
+    floorNormal = floorNormal / norm(floorNormal);
+    wallNormal = wallNormal / norm(wallNormal);
+
+    % Compute constant d
+    floord = -dot(floorNormal, floorCentroid);
+    walld = -dot(wallNormal, wallCentroid);
 
     % Fit a 3D plane to the points
-    floorPlane = [normalVector, d];
-    wallPlane = [normalVectorW, dW];
+    floorPlane = [floorNormal', floord];
+    wallPlane = [wallNormal', walld];
+
+    % Display floor plane equation
+    fprintf('Floor plane: %.3fx + %.3fy + %.3fz + %.3f = 0\n', floorPlane);
 
     % Verify the floor plane is ~ Z=0
-    % The floor plane has a normal vector of [0, 0, 1]
-    % Check if the computed normal vector is ~ [0, 0, 1]
-    angle = acosd(dot(normalVector, [0, 0, 1]));
-    angleW = acosd(dot(normalVectorW, [1, 0, 0]));
-    % Input eq & better validation
-    fprintf('Computed Normal Vector Angle Difference from Vertical: %.3f degrees\n', angle);
-    fprintf('Computed Normal Vector Angle Difference from Vertical: %.3f degrees\n', angleW);
+    a = -floorPlane(1) / floorPlane(3);
+    b = -floorPlane(2) / floorPlane(3);
+    c = -floorPlane(4) / floorPlane(3);
+    fprintf('Floor plane expressed as Z = %.3fx + %.3fy + %.3f\n', a, b, c);
+
+    % While Z != 0, the normal vector is nearly [0,0,1]
+    % Meaning the floor plane is nearly horizontal
     
-    % Wall Plane Equation
-    fprintf('Wall Plane equation: %.3f*X + %.3f*Y + %.3f*Z + %.3f = 0\n', ...
-        normalVectorW(1,1), normalVectorW(1,2), normalVectorW(1,3), dW);
-
-
 
  
 end
