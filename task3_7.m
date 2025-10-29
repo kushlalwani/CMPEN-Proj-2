85c408beaa8956ca686d8f1f42bc5d3ce1cc6091
Ashwin Honnawarkar <ashwin@Ashwins-MacBook-Air.local>
2025-10-29 16:00:22 -0400

    working 3.6

--- task3_7.m
+++ task3_7.m
@@ -2,7 +2,7 @@ function sed = task3_7(pts1, pts2, F)
     N = size(pts1, 1);
     sed = 0;
     for i = 1:N
-        x1 = [pts1(i,:) 1]';  % homogeneous
+        x1 = [pts1(i,:) 1]';  % homogeneous.
         x2 = [pts2(i,:) 1]';
 
         l2 = F * x1;  % epipolar line in image 2
